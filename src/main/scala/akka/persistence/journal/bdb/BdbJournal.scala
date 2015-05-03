c60ade67078c0587b19073be4e4c526efaa5d2dc
Anton Karamanov <ataraxer@gmail.com>
2015-05-03 11:08:50 +0300

    BdbEnvironment: expose only used fields

--- src/main/scala/akka/persistence/journal/bdb/BdbJournal.scala
+++ src/main/scala/akka/persistence/journal/bdb/BdbJournal.scala
@@ -15,27 +15,30 @@ import scala.collection.immutable.Seq
 
 
 trait BdbEnvironment extends Actor {
-  val config = context.system.settings.config.getConfig("bdb-journal")
-  val journalDir = new File(config.getString("dir"))
-  journalDir.mkdirs()
+  private[bdb] val config = context.system.settings.config.getConfig("bdb-journal")
 
 
-  val envConfig = {
+  private[bdb] val env = {
     import EnvironmentConfig._
 
-    new EnvironmentConfig()
-    .setAllowCreate(true)
-    .setTransactional(true)
-    .setLocking(true)
-    .setConfigParam(CLEANER_THREADS, config.getString("cleaner-threads"))
-    .setConfigParam(ENV_DUP_CONVERT_PRELOAD_ALL, "false")
-    .setConfigParam(LOG_GROUP_COMMIT_INTERVAL, config.getString("group-commit-interval"))
-    .setConfigParam(STATS_COLLECT, config.getString("stats-collect"))
-    .setConfigParam(MAX_MEMORY_PERCENT, config.getString("cache-size-percent"))
+    val journalDir = new File(config.getString("dir"))
+
+    journalDir.mkdirs()
+
+    val envConfig = {
+      new EnvironmentConfig()
+      .setAllowCreate(true)
+      .setTransactional(true)
+      .setLocking(true)
+      .setConfigParam(CLEANER_THREADS, config.getString("cleaner-threads"))
+      .setConfigParam(ENV_DUP_CONVERT_PRELOAD_ALL, "false")
+      .setConfigParam(LOG_GROUP_COMMIT_INTERVAL, config.getString("group-commit-interval"))
+      .setConfigParam(STATS_COLLECT, config.getString("stats-collect"))
+      .setConfigParam(MAX_MEMORY_PERCENT, config.getString("cache-size-percent"))
+    }
+
+    new Environment(journalDir, envConfig)
   }
-
-
-  val env = new Environment(journalDir, envConfig)
 
 
   override def postStop(): Unit = {
