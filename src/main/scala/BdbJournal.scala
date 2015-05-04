edf78cee5bd8e81f474b9da9ae3ffda2e3e29794
Anton Karamanov <ataraxer@gmail.com>
2015-05-04 21:43:00 +0300

    BdbEnvironment: move into a separate file

--- src/main/scala/BdbJournal.scala
+++ src/main/scala/BdbJournal.scala
@@ -1,6 +1,5 @@
 package akka.persistence.journal.bdb
 
-import java.io.File
 import java.nio.ByteBuffer
 
 import akka.actor.Actor
@@ -15,43 +14,9 @@ import scala.collection.mutable
 import scala.collection.immutable.Seq
 
 
-trait BdbEnvironment extends Actor {
-  private[bdb] val config = context.system.settings.config.getConfig("bdb-journal")
-
-
-  private[bdb] val env = {
-    import EnvironmentConfig._
-
-    val journalDir = new File(config.getString("dir"))
-
-    journalDir.mkdirs()
-
-    val envConfig = {
-      new EnvironmentConfig()
-      .setAllowCreate(true)
-      .setTransactional(true)
-      .setLocking(true)
-      .setConfigParam(CLEANER_THREADS, config.getString("cleaner-threads"))
-      .setConfigParam(ENV_DUP_CONVERT_PRELOAD_ALL, "false")
-      .setConfigParam(LOG_GROUP_COMMIT_INTERVAL, config.getString("group-commit-interval"))
-      .setConfigParam(STATS_COLLECT, config.getString("stats-collect"))
-      .setConfigParam(MAX_MEMORY_PERCENT, config.getString("cache-size-percent"))
-    }
-
-    new Environment(journalDir, envConfig)
-  }
-
-
-  override def postStop(): Unit = {
-    env.close()
-    super.postStop()
-  }
-}
-
-
 class BdbJournal
-  extends SyncWriteJournal
-  with BdbEnvironment
+  extends BdbEnvironment("bdb-journal")
+  with SyncWriteJournal
   with BdbReplay
 {
   import BdbClient._
